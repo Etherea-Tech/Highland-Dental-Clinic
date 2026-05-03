a1989411eba90a58d964d628e6de2f7e0aa33307
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-05-03 11:15:55 +0530

    services updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -45,7 +45,7 @@ const serviceCategories = [
     items: [
       { name: "Braces", href: "/services/braces", icon: <AlignJustify className="w-4 h-4 text-lightblue" /> },
       { name: "Invisalign", href: "/services/invisalign", icon: <Smile className="w-4 h-4 text-lightblue" /> },
-      { name: "Myobrace", href: "/services/myobrace", icon: <Shield className="w-4 h-4 text-lightblue" /> },
+      
     ]
   },
   {
@@ -54,6 +54,7 @@ const serviceCategories = [
       { name: "TMJ/TMD Treatment", href: "/services/tmj-tmd-treatment", icon: <Activity className="w-4 h-4 text-lightblue" /> },
       { name: "Oral Appliance Therapy for Snoring & Sleep Apnea", href: "/services/oral-appliance-therapy", icon: <Wind className="w-4 h-4 text-lightblue" /> },
       { name: "Bruxism", href: "/services/bruxism", icon: <Moon className="w-4 h-4 text-lightblue" /> },
+      { name: "Myobrace", href: "/services/myobrace", icon: <Shield className="w-4 h-4 text-lightblue" /> },
     ]
   },
   {
